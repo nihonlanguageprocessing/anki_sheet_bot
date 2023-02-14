6e7946ae3c00b65ac83e9c5faaeb4376f8457981
Nathan Port <nathan.port@yahoo.com>
2023-02-14 23:27:11 +0900

    changing functionality to add multiple notes 1-1 and log erros for future web interface

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from google_sheet import get_anki_params, google_sheets
 from google_service import get_service
 from anki_api import invoke
 
+
+## to do, create ability to read row 1, 1, return error message
 if __name__ == '__main__':
     spreadsheet_id = '1Vh8IB6pyUSgff-SaTmsIrOsTlrL8-NQGtlil_FhOrIk'
     sheet_id_from = 64971627
@@ -12,13 +14,21 @@ if __name__ == '__main__':
     header, _, notes = google_sheets(service, get_anki_params, spreadsheet_id, sheet_id_from)
 
     idx = get_idx(header)
-    note = get_note(idx, notes[0])
-    print(note)
 
-    #result = invoke('addNote', note=note)
+
+    for note_ in notes:
+        note = get_note(idx, note_)
+        try:
+            result = invoke('addNote', note=note)
+            print(result)
+
+        except Exception as e:
+            print(e)
+            print(notes)
+
     result = invoke("sync")
 
-    print(result)
+
 
 
 
